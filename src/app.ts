fe94bfc4f30554a20ac56844a685cb2982b20bec
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2024-04-30 20:19:48 -0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express, { json } from "express";
 import { restaurantRouter } from "./routes/restaurant.routes";
 import { HandleErrors } from "./middleware/handleErrors.middleware";
+import cors from "cors";
 
 export const app = express();
+
+app.use(cors());
 
 app.use(json());
 
